0642fbfc28a5c26390f3316846d69173669f0b5d
tpalayda <taras.palayda@gmail.com>
2018-04-25 16:10:43 +0200

    checking sth

--- app/src/main/java/tpalayda/barcodescanner/MenuActivity.java
+++ app/src/main/java/tpalayda/barcodescanner/MenuActivity.java
@@ -2,12 +2,24 @@ package tpalayda.barcodescanner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private Button m_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        m_button = findViewById(R.id.tes2);
+        m_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MenuActivity.this,"clicked",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
