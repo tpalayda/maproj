55d5aab88699d11f16c54b30faa9ec7e7a5aa453
tpalayda <taras.palayda@gmail.com>
2018-05-02 13:37:45 +0200

    fragment for barcodescanner

--- app/src/main/java/tpalayda/barcodescanner/MenuActivity.java
+++ app/src/main/java/tpalayda/barcodescanner/MenuActivity.java
@@ -1,5 +1,8 @@
 package tpalayda.barcodescanner;
 
+import android.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,6 +22,9 @@ public class MenuActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MenuActivity.this,"clicked",Toast.LENGTH_LONG).show();
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                BarcodeFragment bf = new BarcodeFragment();
+                ft.commit();
             }
         });
     }
