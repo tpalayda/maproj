4f5904ce6dc16754526e95000f6e9bd4c46bad88
tpalayda <taras.palayda@gmail.com>
2018-05-12 20:40:54 +0200

    spinner

--- app/src/main/java/tpalayda/barcodescanner/BlankActivity.java
+++ app/src/main/java/tpalayda/barcodescanner/BlankActivity.java
@@ -4,7 +4,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.text.DateFormat;
@@ -14,16 +22,24 @@ public class BlankActivity extends AppCompatActivity {
 
     private EditText m_id;
     private EditText m_date;
+    private EditText m_product;
+    private Spinner  m_category;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blank);
 
-        m_id = findViewById(R.id.barcode_id);
-        m_date = findViewById(R.id.date_id);
+        m_id       = findViewById(R.id.barcode_id);
+        m_date     = findViewById(R.id.date_id);
+        m_product  = findViewById(R.id.product_id);
+        m_category = findViewById(R.id.category_id);
 
+        String[] items = new String[]{"products","technology","other"};
+
+        m_category.setAdapter(new ArrayAdapter<>(this,android.R.layout.simple_spinner_dropdown_item,items));
         m_id.setText("BarcodeID:"+getIntent().getStringExtra("111"));
-        m_date.setText(DateFormat.getDateInstance().format(new Date()));
+        m_date.setText("Date:"+DateFormat.getDateInstance().format(new Date()));
     }
 }
